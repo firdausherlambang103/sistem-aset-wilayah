c4796d0087a38d8a0c5ba79038d58187ef81a870
firdausherlambang103 <firdausherlambang18@gmail.com>
2026-06-29 15:31:49 +0700

    update

--- routes/auth.php
+++ routes/auth.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\PasswordController; // <-- Tambahan Import
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -15,6 +16,9 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    // Ubah Password (Fitur Baru)
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
     // Logout
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
